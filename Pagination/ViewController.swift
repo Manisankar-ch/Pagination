4b9dbcaf792c929dbd41aa4b49a36a9e92308b21
Manisankar <msshankar.ch@gmail.com>
2025-02-10 11:11:16 +0530

    Initial Commit

---
+++ Pagination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pagination
+//
+//  Created by Softsuave-iOS dev on 10/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
